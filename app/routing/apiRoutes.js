4e1eb2a7428b3dee20328445bc7d07959f6ae0e4
cbrenner04 <christopher.brenner1@northwestern.edu>
2017-01-14 21:18:37 -0600

    Set up basic file structure and routing

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+
+var router = express.Router();
+
+var friends = [];
+
+// define the get api/friends route
+router.get('/api/friends', function(req, res) {
+    res.json(friends);
+});
+
+// define the about route
+router.post('/api/friends', function(req, res) {
+    console.log('HIT THE POST');
+});
+
+module.exports = router;
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var path = require('path');
+
+var router = express.Router();
+
+// define the home page route
+router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/home.html'));
+});
+
+// define the about route
+router.get('/survey', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/survey.html'));
+});
+
+module.exports = router;
