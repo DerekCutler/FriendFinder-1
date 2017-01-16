bec1e83ea2c8be271d0e47dec5251aff8e62ee07
cbrenner04 <christopher.brenner1@northwestern.edu>
2017-01-16 10:28:55 -0600

    Minor fix and clean

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,15 @@
 var express = require('express');
 var path = require('path');
+var friends = require('../data/friends.js');
 
 var router = express.Router();
-
-var friends = [];
 
 // define the get api/friends route
 router.get('/api/friends', function(req, res) {
     res.json(friends);
 });
 
-// define the about route
+// define the post api/friends route
 router.post('/api/friends', function(req, res) {
     friends.push(req.body);
 });
